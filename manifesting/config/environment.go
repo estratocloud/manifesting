ab8dcacb54171f90fd3dad881e7b7644a81c0167
Craig Duncan <craig.duncan@estrato.co.uk>
2026-07-16 12:22:47 +0100

    Add a GetOutputPath to the Environment (#21)

--- manifesting/config/environment.go
+++ manifesting/config/environment.go
@@ -1,7 +1,20 @@
 package config
 
+import (
+	"fmt"
+
+	"github.com/estratocloud/manifesting/internal"
+)
+
 type Environment struct {
 	Name    string `yaml:"name"`
 	Output  string `yaml:"output"`
 	EnvFrom string `yaml:"envFrom"`
+}
+
+func (e *Environment) GetOutputPath(wd internal.WorkingDirectoryInterface) internal.PathInterface {
+	if e.Output == "" {
+		return wd.NewPath(fmt.Sprintf(".generated/%s.yaml", e.Name))
+	}
+	return wd.NewPath(e.Output)
 }
